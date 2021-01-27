55f045a13c85ea1a78bdbe166f32eff4d9992d70
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-01-26 22:31:56 -0300

    0.0.3 - Configurations

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour {
+
+    private void Update() {
+        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
