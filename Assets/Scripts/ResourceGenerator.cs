b24f6dd6ae75ade03f8713f3edace3d2dc25aab6
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-01-28 19:54:02 -0300

    0.0.7 - Resoruce Generator

---
+++ Assets/Scripts/ResourceGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGenerator : MonoBehaviour {
+
+    private float timer;
+    private float timerMax;
+
+    private void Awake() {
+        timerMax = 1f;
+    }
+
+    private void Update() {
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f) {
+            timer += timerMax;
+            Debug.Log("DING!");
+        }
+    }
+}
