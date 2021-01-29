0c360c070f74493e0fcc60af2fdf604b943d7d18
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-01-29 15:43:52 -0300

    0.0.11 - Basic Cameta Controls

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraHandler : MonoBehaviour {
+
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private float orthographicSize;
+    private float targetOrthographicSize;
+
+
+    private void Start() {
+        orthographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        targetOrthographicSize = orthographicSize;
+    }
+
+    private void Update() {
+        HandleMovement();
+        HandleZoom();
+    }
+
+    private void HandleMovement() {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDirection = new Vector3(x, y).normalized;
+
+        float moveSpeed = 30f;
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            moveSpeed = 60f;
+        }
+
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom() {
+
+        float zoomAmount = 2f;
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            zoomAmount = 4f;
+        }
+
+        targetOrthographicSize += -Input.mouseScrollDelta.y * zoomAmount;
+
+        float minOrthographicSize = 10;
+        float maxOrthographicSize = 30;
+
+        targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, minOrthographicSize, maxOrthographicSize);
+
+        float zoomSpeed = 5f;
+        orthographicSize = Mathf.Lerp(orthographicSize, targetOrthographicSize, Time.deltaTime * zoomSpeed);
+
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
+    }
+}
