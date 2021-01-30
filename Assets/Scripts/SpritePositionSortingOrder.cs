31e41bf0d1ee746d62c27057355c8b289019f0f8
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-01-30 15:05:20 -0300

    0.0.17 - Node Creation

---
+++ Assets/Scripts/SpritePositionSortingOrder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritePositionSortingOrder : MonoBehaviour {
+
+    [SerializeField] private bool runOnce;
+    private SpriteRenderer spriteRenderer;
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void LateUpdate() {
+        float precisionMultiplier = 5f;
+        spriteRenderer.sortingOrder = -(int)(transform.position.y * precisionMultiplier);
+
+        if (runOnce) {
+            Destroy(this);
+        }
+    }
+}
