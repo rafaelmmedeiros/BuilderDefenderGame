c48f3b5f93a0846846f39c154b666f1ec61ccdbf
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-02-04 22:02:30 -0300

    Hotfix

--- Assets/Scripts/EnemyWaveManager.cs
+++ Assets/Scripts/EnemyWaveManager.cs
@@ -28,7 +28,7 @@ public class EnemyWaveManager : MonoBehaviour {
         state = State.WaitingToSpawnNextWave;
         spawnPosition = spawnPositionTransformList[UnityEngine.Random.Range(0, spawnPositionTransformList.Count)].position;
         nextWaveSpawnPositionTransform.position = spawnPosition;
-        nextWaveSpawnTimer = 3f;
+        nextWaveSpawnTimer = 20f;
     }
 
     private void Update() {
@@ -51,7 +51,7 @@ public class EnemyWaveManager : MonoBehaviour {
                             state = State.WaitingToSpawnNextWave;
                             spawnPosition = spawnPositionTransformList[UnityEngine.Random.Range(0, spawnPositionTransformList.Count)].position;
                             nextWaveSpawnPositionTransform.position = spawnPosition;
-                            nextWaveSpawnTimer = 10f;
+                            nextWaveSpawnTimer = 20f;
                         }
                     }
                 }
--- Assets/Scripts/EnemyWaveUI.cs
+++ Assets/Scripts/EnemyWaveUI.cs
@@ -12,11 +12,14 @@ public class EnemyWaveUI : MonoBehaviour {
     private TextMeshProUGUI waveNumberText;
     private TextMeshProUGUI waveMessageText;
     private RectTransform enemyWaveSpawnPositionIndicator;
+    private RectTransform enemyClosestPositionIndicator;
+
 
     private void Awake() {
         waveNumberText = transform.Find("waveNumberText").GetComponent<TextMeshProUGUI>();
         waveMessageText = transform.Find("waveMessageText").GetComponent<TextMeshProUGUI>();
         enemyWaveSpawnPositionIndicator = transform.Find("enemyWaveSpawnPositionIndicator").GetComponent<RectTransform>();
+        enemyClosestPositionIndicator = transform.Find("enemyClosestPositionIndicator").GetComponent<RectTransform>();
     }
 
     private void Start() {
@@ -30,14 +33,12 @@ public class EnemyWaveUI : MonoBehaviour {
     }
 
     private void Update() {
-        float nextWaveSpawnTimer = enemyWaveManager.GetNextWaveSpawnTimer();
+        HandleNextWaveMessage();
+        HandleEnemyWaveSpawnPositionIndicator();
+        HandleEnemyClosestPositionIndicator();
+    }
 
-        if (nextWaveSpawnTimer <= 0f) {
-            SetMessageText("");
-        } else {
-            SetMessageText($"Next Wave in {nextWaveSpawnTimer:F1}s");
-        }
-
+    private void HandleEnemyWaveSpawnPositionIndicator() {
         Vector3 directionToNextSpawnPosition = (enemyWaveManager.GetSpawnPosition() - mainCamera.transform.position).normalized;
 
         enemyWaveSpawnPositionIndicator.anchoredPosition = directionToNextSpawnPosition * 300f;
@@ -45,6 +46,54 @@ public class EnemyWaveUI : MonoBehaviour {
 
         float distanceToNextSpawPosition = Vector3.Distance(enemyWaveManager.GetSpawnPosition(), mainCamera.transform.position);
         enemyWaveSpawnPositionIndicator.gameObject.SetActive(distanceToNextSpawPosition > mainCamera.orthographicSize * 1.5f);
+
+    }
+
+    private void HandleNextWaveMessage() {
+        float nextWaveSpawnTimer = enemyWaveManager.GetNextWaveSpawnTimer();
+
+        if (nextWaveSpawnTimer <= 0f) {
+            SetMessageText("");
+        } else {
+            SetMessageText($"Next Wave in {nextWaveSpawnTimer:F1}s");
+        }
+    }
+
+    private void HandleEnemyClosestPositionIndicator() {
+        float targetMaxRadius = 9999f;
+        Collider2D[] targetsInRangeArray = Physics2D.OverlapCircleAll(mainCamera.transform.position, targetMaxRadius);
+
+        Enemy targetEnemy = null;
+
+        foreach (Collider2D target in targetsInRangeArray) {
+            Enemy enemy = target.GetComponent<Enemy>();
+            if (enemy != null) {
+                // It´s a ENEMY!! It will become the new Target!! :)
+                if (targetEnemy == null) {
+                    targetEnemy = enemy;
+                } else {
+                    // Compare distance and pick the closest.
+                    if (Vector3.Distance(transform.position, enemy.transform.position)
+                    < Vector3.Distance(transform.position, targetEnemy.transform.position)) {
+                        // This is the closers enemy! Time to Attack!
+                        targetEnemy = enemy;
+                    }
+                }
+            }
+        }
+
+        if (targetEnemy != null) {
+            Vector3 directionToClosestEnemy = (targetEnemy.transform.position - mainCamera.transform.position).normalized;
+
+            enemyClosestPositionIndicator.anchoredPosition = directionToClosestEnemy * 250f;
+            enemyClosestPositionIndicator.eulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVector(directionToClosestEnemy));
+
+            float distanceToClosestEnemy = Vector3.Distance(targetEnemy.transform.position, mainCamera.transform.position);
+            enemyClosestPositionIndicator.gameObject.SetActive(distanceToClosestEnemy > mainCamera.orthographicSize * 1.5f);
+        } else {
+            // There is no enemies alive.
+            enemyClosestPositionIndicator.gameObject.SetActive(false);
+        }
     }
 
     private void SetWaveNumberText(string waveNUmber) {
